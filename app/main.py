fbbb33d392c1d925b98c4c869ca2f61c48a1c0f2
sharath-mohan <sharathdesignsofficial@gmail.com>
2025-09-15 14:49:19 +0530

    fix: added CORS

--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 from app.routers import items
 from app.database import get_db
 
+origins = [
+    "http://localhost:5123",
+]
 
 
 app = FastAPI(title="My API",)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"], 
+    allow_headers=["*"],
+)
 
 get_db()
 
